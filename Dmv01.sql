83a6794af44099998c72a88b5544dd3e7d7a3d71
Dennis Wells <aadennis@users.noreply.github.com>
2018-12-13 08:40:09 +0000

    Update Dmv01.sql

--- Dmv01.sql
+++ Dmv01.sql
@@ -10,8 +10,8 @@ SUM(s.elapsed_time) sum_total_elapsed_time
 SUM(s.total_worker_time) sum_total_worker_time 
 SUM(s.tota1_logical_reads) sum total logical reads 
 SUM(s.tota1_logical_writes) sum_total_logical_writes 
-FROM sys. dm exec_query_stats s 
-CROUP BY s.plan_handle 
+FROM sys.dm_exec_query_stats s 
+GROUP BY s.plan_handle 
 ) AS ss 
-CROSS APPLY sys.dm exec_sql_text(ss.plan handle) t 
-ORDER BY sum total_logical reads DESC 
+CROSS APPLY sys.dm_exec_sql_text(ss.plan handle) t 
+ORDER BY sum_total_logical_reads DESC 
