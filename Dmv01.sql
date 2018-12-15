9ee6f4a219dbf5aef135b1cea29c41c514afc4da
Dennis Wells <aadennis@users.noreply.github.com>
2018-12-15 19:05:51 +0000

    Update Dmv01.sql

--- Dmv01.sql
+++ Dmv01.sql
@@ -1,17 +1,21 @@
-SELECT ss.sum_execution_count 
-,t.TEXT 
-,ss.sum_total_elapsed_time 
-,ss.sum_total_worker_time 
-,ss.sum_total_logical_reads 
-,ss.sum_total_logical_writes 
+with dmv_sums as (
+SELECT 
+ t.dbid, DB_NAME(t.dbid) dbname
+,t.TEXT
+,ss.sum_execution_count 
+,ss.sum_total_elapsed_time stel
+,ss.sum_total_worker_time stwt
+,ss.sum_total_logical_reads  stlr
+,ss.sum_total_logical_writes  stlw
 FROM (SELECT s.plan_handle 
 ,SUM(s.execution_count) sum_execution_count 
-SUM(s.elapsed_time) sum_total_elapsed_time 
-SUM(s.total_worker_time) sum_total_worker_time 
-SUM(s.tota1_logical_reads) sum total logical reads 
-SUM(s.tota1_logical_writes) sum_total_logical_writes 
+,SUM(s.total_elapsed_time) sum_total_elapsed_time 
+,SUM(s.total_worker_time) sum_total_worker_time 
+,SUM(s.total_logical_reads) sum_total_logical_reads 
+,SUM(s.total_logical_writes) sum_total_logical_writes 
 FROM sys.dm_exec_query_stats s 
 GROUP BY s.plan_handle 
 ) AS ss 
-CROSS APPLY sys.dm_exec_sql_text(ss.plan handle) t 
-ORDER BY sum_total_logical_reads DESC 
+CROSS APPLY sys.dm_exec_sql_text(ss.plan_handle) t 
+) select *  from dmv_sums
+ORDER BY stlr DESC 
