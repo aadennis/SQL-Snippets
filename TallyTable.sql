a1fe27c97212c48d957422474f3568d93f152071
Dennis Wells <aadennis@users.noreply.github.com>
2018-12-24 17:02:09 +0000

    Update TallyTable.sql

--- TallyTable.sql
+++ TallyTable.sql
@@ -10,3 +10,16 @@ from master.sys.all_columns a cross apply master.sys.all_columns b)
 insert into InstanceStatistics.dbo.TallyTable (col2) select null from x;
 -- check
 select count(1) from InstanceStatistics.dbo.TallyTable
+
+-- And example usage... 
+-- The top 100 n part must of course be <= the total in the Tally Table
+
+drop table InstanceStatistics.dbo.SomeBusinessTable;
+create table InstanceStatistics.dbo.SomeBusinessTable (SupplierId bigint primary key, SupplierName nvarchar(1000));
+
+insert into InstanceStatistics.dbo.SomeBusinessTable
+(SupplierId, SupplierName)
+select top 100 col1, 'Supplier name ' +convert(nvarchar(1000), col1)
+from InstanceStatistics.dbo.TallyTable
+
+select * from InstanceStatistics.dbo.SomeBusinessTable;
